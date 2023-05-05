604c58a0bd8c10228f1ee79d449d514fd6b68b3a
Leo Leplat <leo.leplat@proton.me>
2023-05-05 11:24:02 +0100

    Command line parameters and UX
    Added support for command line parameters: the application must be started with "-t" or "--test" to display the random log messages. Adding "-h" or "--help" displays a help message.
    
    Improved UX: Level buttons are now defined in FXML, some space was added between them, their font size has been decreased, they are now located on the right of the filter box, the filter box fills all the remaining space, log messages can be copied with the right click button, and the footer now displays a more concise text.

--- log-viewer-app/src/main/java/io/github/qupath/logviewer/app/LogViewerApp.java
+++ log-viewer-app/src/main/java/io/github/qupath/logviewer/app/LogViewerApp.java
@@ -10,14 +10,16 @@ import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
-
-import java.util.Objects;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class LogViewerApp extends Application {
     private final static Logger logger = LoggerFactory.getLogger(LogViewerApp.class);
+    private ScheduledExecutorService executor;
 
     public static void main(String[] args) {
         Application.launch(LogViewerApp.class, args);
@@ -36,32 +38,53 @@ public class LogViewerApp extends Application {
         loader.load();
         Parent root = loader.getRoot();
         Scene scene = new Scene(root);
-        String stylesheet = Objects.requireNonNull(getClass().getResource("css/styles.css")).toExternalForm();
-        scene.getStylesheets().add(stylesheet);
 
         primaryStage.setTitle(resources.getString("title"));
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        scene.addEventHandler(MouseEvent.ANY, LogViewerApp::logMouseEvent);
+        handleParameters(scene);
+    }
 
-        logger.info("Here's my first log message, for information");
-        try {
-            throw new RuntimeException("Here is a runtime exception");
-        } catch (Exception e) {
-            logger.error("Exception", e);
+    @Override
+    public void stop() {
+        if (executor != null) {
+            executor.shutdown();
         }
-        Platform.runLater(() -> logRandomMessages(1000));
-        logRandomMessages(1000);
-        logger.warn("Here's a final message. With a warning.");
+    }
+
+    private void handleParameters(Scene scene) {
+        Parameters parameters = getParameters();
+
+        if (parameters.getRaw().contains("-h") || parameters.getRaw().contains("--help")) {
+            logger.info("Options:");
+            logger.info("-t, --test           Log random messages");
+            logger.info("-h, --help           Displays this help message");
+            System.exit(0);
+        }
+
+        if (parameters.getRaw().contains("-t") || parameters.getRaw().contains("--test")) {
+            scene.addEventHandler(MouseEvent.ANY, LogViewerApp::logMouseEvent);
+
+            logger.info("Here's my first log message, for information");
+            try {
+                throw new RuntimeException("Here is a runtime exception");
+            } catch (Exception e) {
+                logger.error("Exception", e);
+            }
+            Platform.runLater(() -> logRandomMessages(1000));
+            logRandomMessages(1000);
+            logger.warn("Here's a final message. With a warning.");
+
+            executor = Executors.newScheduledThreadPool(1);
+            executor.scheduleAtFixedRate(() -> logSingleRandomMessage(-1), 0, 1, TimeUnit.SECONDS);
+        }
     }
 
     private static void logMouseEvent(MouseEvent event) {
-        if (event.getEventType() == MouseEvent.MOUSE_MOVED || event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
-//            logger.trace("Mouse event: {} at ({}, {})", event.getEventType(), event.getX(), event.getY());
-            return;
+        if (event.getEventType() != MouseEvent.MOUSE_MOVED && event.getEventType() != MouseEvent.MOUSE_DRAGGED) {
+            logger.info("Mouse event: {} at ({}, {})", event.getEventType(), event.getX(), event.getY());
         }
-        logger.info("Mouse event: {} at ({}, {})", event.getEventType(), event.getX(), event.getY());
     }
 
     private static void logRandomMessages(int maxMessages) {
